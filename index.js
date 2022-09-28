1f8d636c7fb8feab12110a45456671bc43db178f
Donald Geddes <hi@donaldgeddes.ca>
2022-09-28 15:39:57 -0700

    Use absolute paths so that npx cooperates

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
 
+import { _dirname, join } from '@hbauer/convenience-functions'
 import { createAddressRecord, getSubdomain } from './src/index.js'
 
 const stdout = console.log
+const dirname = _dirname(import.meta.url)
 
 // Wrapped because CJS build complains otherwise
 async function run() {
   const targetDomain = process.env.TARGET_DOMAIN
-  const subdomain = await getSubdomain(targetDomain, './src/five-letter-words')
+  const pathToFiveLetterWords = join(dirname, 'src/five-letter-words')
+  const subdomain = await getSubdomain(targetDomain, pathToFiveLetterWords)
   const { status, message } = await createAddressRecord(targetDomain, subdomain)
   stdout(status === 'SUCCESS' ? true : message)
 }
