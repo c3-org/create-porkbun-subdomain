63d4000de2a102585cd07536f3c51e4020ec6f12
Donald Geddes <hi@donaldgeddes.ca>
2022-09-28 16:10:22 -0700

    Modify sdout to include more detail

--- index.js
+++ index.js
@@ -9,10 +9,17 @@ const dirname = _dirname(import.meta.url)
 // Wrapped because CJS build complains otherwise
 async function run() {
   const targetDomain = process.env.TARGET_DOMAIN
+
   const pathToFiveLetterWords = join(dirname, 'src/five-letter-words')
   const subdomain = await getSubdomain(targetDomain, pathToFiveLetterWords)
   const { status, message } = await createAddressRecord(targetDomain, subdomain)
-  stdout(status === 'SUCCESS' ? true : message)
+
+  const info = {
+    success: status === 'SUCCESS' && true,
+    message,
+    domain: subdomain + targetDomain,
+  }
+  stdout(JSON.stringify(info))
 }
 
 run()
