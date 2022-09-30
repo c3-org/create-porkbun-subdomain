9129734474115d523747e10a1bf9f20523ae7dbb
Donald Geddes <hi@donaldgeddes.ca>
2022-09-30 16:55:08 -0700

    Remove log

--- src/index.js
+++ src/index.js
@@ -12,8 +12,6 @@ function getInstanceNames(targetDomain) {
     path: `api/json/v3/dns/retrieve/${targetDomain}`,
   })
 
-  console.log(url)
-
   /**
    * @param {{ records: { name: string, type: string }[] }} param0
    * @returns
